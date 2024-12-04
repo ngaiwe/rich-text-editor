6555ee3263202d9393a29ad8712b637a78fa80da
weiran01 <weiran01@zhihu.com>
2024-12-04 16:21:43 +0800

    feat: 增加prettier 配置

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,17 +1,4 @@
 module.exports = {
-  bracketSpacing: false,
-  semi: false,
-  singleQuote: true,
-  trailingComma: 'es5',
-  arrowParens: 'avoid',
-  overrides: [
-    // yarn berry 配置文件使用双引号
-    {
-      files: ['.yarnrc.yml'],
-      options: {
-        singleQuote: false,
-      },
-    },
-  ],
+  ...require('zhihu-lint/.prettierrc'),
   plugins: ['prettier-plugin-organize-imports'],
 }
